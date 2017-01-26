9525448666a041832553fa6d78d567a1a0ce776f
stalbert <stalbert@exploreconsulting.com>
2017-01-26 06:36:03 -0800

    adding a file to capture code snippets used in the overview documentation

---
+++ IntroGuideExamples.ts
@@ -0,0 +1,43 @@
+
+//region logging example
+declare var log
+declare var LogManager
+
+var customer = nlapiLoadRecord('customer', '1234')
+
+var comments = customer.getFieldValue('comments')
+var isinactive = customer.getFieldValue('isinactive')
+var phone = customer.getFieldValue('phone')
+
+nlapiLogExecution("DEBUG", "customer info", "comments:" + comments + " isinactive:" + isinactive + " phone:" + phone)
+
+
+namespace NFT {
+
+    var customer = nsdal.loadObject('customer', '1234', ['comments', 'isinactive', 'phone'])
+
+    log.debug("customer info", customer)
+
+
+
+    // MyComponent uses a custom logger, whose loglevel can be controlled independently of the default logger
+    class MyComponent {
+        log = LogManager.getLogger('MyComponent')
+        constructor () {
+            this.log.debug('constructing MyComponent')
+        }
+
+        foo() {
+            this.log.error('failure!')
+        }
+    }
+    // set MyComponent to only log 'warning' level and above
+    var c = new MyComponent()
+    c.log.setLevel(LogManager.logLevel.warn)
+
+    // use the component
+    c.foo()
+}
+//endregion
+
+
