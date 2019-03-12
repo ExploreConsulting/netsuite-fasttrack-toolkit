77f1fb6d33ffdae2d029b3111afb28bc51fdcfc8
Shawn Talbert <Shawn.Talbert@rsmus.com>
2019-03-12 03:25:26 -0700

    hack lazyjs and moment to bypass NetSuite 2019.1 bug where it falsely identifies as an AMD environment for SS1 scripts.

--- Aurelia-Logger-webpack-lib.js
+++ Aurelia-Logger-webpack-lib.js
@@ -216,67 +216,29 @@ var Logger = exports.Logger = function () {
 
 /***/ }),
 /* 1 */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ConsoleAppender", function() { return ConsoleAppender; });
 
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.ConsoleAppender = undefined;
-
-var _aureliaLogging = __webpack_require__(0);
-
-
-
-var ConsoleAppender = exports.ConsoleAppender = function () {
-  function ConsoleAppender() {
-    
+let ConsoleAppender = class ConsoleAppender {
+  debug(logger, ...rest) {
+    console.debug(`DEBUG [${logger.id}]`, ...rest);
   }
 
-  ConsoleAppender.prototype.debug = function debug(logger) {
-    var _console;
+  info(logger, ...rest) {
+    console.info(`INFO [${logger.id}]`, ...rest);
+  }
 
-    for (var _len = arguments.length, rest = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-      rest[_key - 1] = arguments[_key];
-    }
+  warn(logger, ...rest) {
+    console.warn(`WARN [${logger.id}]`, ...rest);
+  }
 
-    (_console = console).debug.apply(_console, ['DEBUG [' + logger.id + ']'].concat(rest));
-  };
-
-  ConsoleAppender.prototype.info = function info(logger) {
-    var _console2;
-
-    for (var _len2 = arguments.length, rest = Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
-      rest[_key2 - 1] = arguments[_key2];
-    }
-
-    (_console2 = console).info.apply(_console2, ['INFO [' + logger.id + ']'].concat(rest));
-  };
-
-  ConsoleAppender.prototype.warn = function warn(logger) {
-    var _console3;
-
-    for (var _len3 = arguments.length, rest = Array(_len3 > 1 ? _len3 - 1 : 0), _key3 = 1; _key3 < _len3; _key3++) {
-      rest[_key3 - 1] = arguments[_key3];
-    }
-
-    (_console3 = console).warn.apply(_console3, ['WARN [' + logger.id + ']'].concat(rest));
-  };
-
-  ConsoleAppender.prototype.error = function error(logger) {
-    var _console4;
-
-    for (var _len4 = arguments.length, rest = Array(_len4 > 1 ? _len4 - 1 : 0), _key4 = 1; _key4 < _len4; _key4++) {
-      rest[_key4 - 1] = arguments[_key4];
-    }
-
-    (_console4 = console).error.apply(_console4, ['ERROR [' + logger.id + ']'].concat(rest));
-  };
-
-  return ConsoleAppender;
-}();
+  error(logger, ...rest) {
+    console.error(`ERROR [${logger.id}]`, ...rest);
+  }
+};
 
 /***/ }),
 /* 2 */
--- lazy-0.4.3.js
+++ lazy-0.4.3.js
@@ -60,7 +60,7 @@
  */
 
 (function(root, factory) {
-  if (typeof define === 'function' && define.amd) {
+  if (false) {
     define(factory);
   } else if (typeof exports === 'object') {
     module.exports = factory();
